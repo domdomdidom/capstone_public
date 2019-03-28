089620080ab68b05601e3f46a10309e3d07e71d8
domdomdidom <domvandendries@gmail.com>
2019-03-28 16:10:31 -0700

    thurs, may 28

--- project_functions.py
+++ project_functions.py
@@ -5,8 +5,10 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import NMF as NMF_sklearn
 import warnings; warnings.simplefilter('ignore')
+import rfpimp
+from sklearn.metrics import confusion_matrix, precision_score, recall_score
 
-custy_df = pd.read_csv('custy_data.csv', skiprows=None, index_col='Customer ID')
+custy_df = pd.read_csv('custy_data.csv', skiprows=None, index_col='Customer ID', parse_dates=["Date Joined"])
 custy_df = custy_df.dropna(axis=0, how='all')
 custy_df = custy_df.drop(columns=['Rep Name', 'Education'], axis=1)
 order_df = pd.read_csv('order_data.csv', parse_dates=["Order Date"])
@@ -16,7 +18,7 @@ subscriber_df = pd.read_csv('subscribers-2019-03-27.csv')
 
 def assemble_feature_df():
     
-    feature_extraction_df = pd.DataFrame(columns=['AOV', 'Lapse', 'Order Freq', 'Total Ship', 'STD', 'AVGPERITEM', 'Is Sub'])
+    feature_extraction_df = pd.DataFrame(columns=['avg_order_value', 'days_since_last_order', 'time_as_customer', 'order_freq', 'n_total_orders', 'ship_total', 'std_order_value', 'avg_price_item', 'subscriber_newsletter'])
     feature_extraction_df = feature_extraction_df.reindex(custy_df.index)
     
     subscriber_emails = list(subscriber_df['Email'])
@@ -26,7 +28,7 @@ def assemble_feature_df():
         today = order_df["Order Date"].max() # this is the last order date we have in the order DF
 
         mask = order_df[order_df['Customer ID'] == customer] # mask for all orders under a customer
-        if len(mask) == 0: # skip this customer if they've never ordered
+        if len(mask) <= 0: # skip this customer if they've never ordered
             pass
 
         else:
@@ -40,6 +42,9 @@ def assemble_feature_df():
 
             # days since last order
             lapse = (today - mask["Order Date"].max()).days
+            
+            # time as customer
+            time_as_customer = (mask["Order Date"].max() - custy_df['Date Joined'][customer]).days
 
             # average order value
             AOV = round(mask['Subtotal'].sum() / len(mask), 3)
@@ -58,20 +63,21 @@ def assemble_feature_df():
             else: is_subscriber = 0
 
             # throw all this info into a matrix indexed by customer id number. i can probably throw this in a for loop to clean it up a bit
+            
+            feature_extraction_df.loc[customer]['avg_order_value'] = AOV
+            feature_extraction_df.loc[customer]['std_order_value'] = std_dev
+            feature_extraction_df.loc[customer]['n_total_orders'] = num_orders
+            feature_extraction_df.loc[customer]['days_since_last_order'] = lapse
+            feature_extraction_df.loc[customer]['time_as_customer'] = time_as_customer
+            feature_extraction_df.loc[customer]['order_freq'] = freq
+            feature_extraction_df.loc[customer]['ship_total'] = total_ship
+            feature_extraction_df.loc[customer]['avg_price_item'] = avg_price_per_item
+            feature_extraction_df.loc[customer]['subscriber_newsletter'] = is_subscriber
 
-            feature_extraction_df.loc[customer]['Order Freq'] = freq
-            feature_extraction_df.loc[customer]['AOV'] = AOV
-            feature_extraction_df.loc[customer]['Total Ship'] = total_ship
-            feature_extraction_df.loc[customer]['Lapse'] = lapse
-            feature_extraction_df.loc[customer]['STD'] = std_dev
-            feature_extraction_df.loc[customer]['AVGPERITEM'] = avg_price_per_item
-            feature_extraction_df.loc[customer]['Is Sub'] = is_subscriber
-
-
-    feature_extraction_df['Customer Group'] = custy_df['Customer Group']
-    feature_extraction_df['Affiliation'] = custy_df['Your Affiliation']
+    feature_extraction_df['customer_group'] = custy_df['Customer Group']
+    feature_extraction_df['affiliation'] = custy_df['Your Affiliation']
     
-    return feature_extraction_df
+    return feature_extraction_df.dropna(thresh=3)
 
 
 def make_historical_purchase_matrix():
@@ -117,31 +123,58 @@ def do_NMF(X, n_topics, max_iters, n_items):
 
 
 def make_churn(df, days):
-    df['churn_bool'] = np.where(df['Lapse']>=days, 1, 0)
+    df['churn_bool'] = np.where(df['days_since_last_order']>=days, 1, 0)
 
     
-def log_cost_feats(df):
+def log_cost_features(df):
     
-    df['AOV'] = df['AOV'].apply(lambda x: np.log(x) if x > 0 else 0)
-    df['Total Ship'] = df['Total Ship'].apply(lambda x: np.log(x) if x > 0 else 0)
-    
-    return df
+    df['avg_order_value_logged'] = df['avg_order_value'].apply(lambda x: np.log(x) if x > 0 else 0)
+    df['ship_total_logged'] = df['ship_total'].apply(lambda x: np.log(x) if x > 0 else 0)
 
+    df = df.drop(columns=['ship_total', 'avg_order_value'], axis=1)
     
+    return df.fillna(0)
+
 def binarize_cols(df):
-    dummy_Cgroup = pd.get_dummies(df['Customer Group'])
-    dummy_aff = pd.get_dummies(df['Affiliation'])
+    dummy_Cgroup = pd.get_dummies(df['customer_group'])
+    dummy_aff = pd.get_dummies(df['affiliation'])
     
     dummy_merged = dummy_aff.merge(dummy_Cgroup, on='Customer ID')
     returned = df.join(dummy_merged)
-    returned2 = returned.drop(columns=['Affiliation', 'Customer Group'], axis=1)
+    returned2 = returned.drop(columns=['affiliation', 'customer_group'], axis=1)
     
     return returned2
     
 def make_TT_split(df):
+
     y = df['churn_bool']
-    X = df.drop(columns=['Lapse', 'churn_bool'], axis=1)
+    X = df.drop(columns=['days_since_last_order', 'churn_bool'], axis=1)
     
     X_train, X_test, y_train, y_test = train_test_split(X, y)
     
     return X_train, X_test, y_train, y_test
+
+def score_rf(df, n_feature_importances):
+    
+    X_train, X_test, y_train, y_test = make_TT_split(df)
+    
+    rf = RandomForestClassifier(oob_score=True)
+    rf.fit(X_train, y_train)
+    y_pred = rf.predict(X_test)
+    imp = rfpimp.importances(rf, X_test, y_test)
+    cols_to_drop = list(imp[imp['Importance'] <= 0].index)
+    
+    if len(cols_to_drop) > 0:
+        df = df.drop(columns=cols_to_drop, axis=1) # drop the unimportant columns, run the model again
+        rf = RandomForestClassifier(oob_score=True)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_test)
+        imp = rfpimp.importances(rf, X_test, y_test)
+        cols_to_drop = list(imp[imp['Importance'] <= 0].index)
+    
+    print(confusion_matrix(y_test, y_pred))
+    print("\naccuracy:", rf.score(X_test, y_test))
+    print("precision:", precision_score(y_test, y_pred))
+    print("recall:", recall_score(y_test, y_pred))
+    print('\n')
+    print(imp[0:n_feature_importances])
